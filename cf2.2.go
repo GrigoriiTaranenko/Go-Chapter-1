66fd4851ff648400cf97cd255365abd939df5795
GRIGORIITARANENKO <grigorii.taranenko@gmail.com>
2017-07-12 10:51:50 +0300

    2.2

--- cf2.2.go
+++ cf2.2.go
@@ -5,18 +5,30 @@ import (
 	"strconv"
 	"fmt"
 	"tempconf"
+	"distanceconf"
+	"weightconf"
 )
 
 func commandLine(arg string, units *map[string]float64) {
-	t, err := strconv.ParseFloat(arg, 64)
+	conf, err := strconv.ParseFloat(arg, 64)
 	if err !=nil {
 		fmt.Fprintf(os.Stderr, "cf: %v\n", err)
 		os.Exit(1)
 	}
-	f := tempconf.Fahrenheit(t)
-	c := tempconf.Celsius(t)
+	f := tempconf.Fahrenheit(conf)
+	c := tempconf.Celsius(conf)
+
+	m := distanceconf.Meter(conf)
+	foot := distanceconf.Foot(conf)
+
+	kg := weightconf.Kg(conf)
+	pound := weightconf.Pound(conf)
 	fmt.Printf("%s = %s %s = %s \n",
 		f, tempconf.FToC(f), c, tempconf.CToF(c))
+	fmt.Printf("%s = %s %s = %s \n",
+		m, distanceconf.MToF(m), foot, distanceconf.FToM(foot))
+	fmt.Printf("%s = %s %s = %s \n",
+		kg, weightconf.KToP(kg), pound, weightconf.PToK(pound))
 }
 
 func main () {
