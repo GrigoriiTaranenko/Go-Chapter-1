d3d585912e33b26222fc7481e25947dcd73871dd
GRIGORIITARANENKO <grigorii.taranenko@gmail.com>
2017-07-14 15:41:46 +0300

    Новые задания

--- surface3.1.go
+++ surface3.1.go
@@ -7,11 +7,11 @@ import (
 
 const (
 	width, height = 600, 320 //размер канвы в пикселях
-	cells         = 100      //Количество ячеек в сетки
+	cells         = 100  //Количество ячеек в сетки
 	xyrange       = 30.0     //Диапозон осей
-	xyscale       = width / 2 / xyrange
-	zscale        = height * 0.4
-	angle         = math.Pi / 6
+	xyscale       = width / 2 / xyrange //Пикселей в единице x или y
+	zscale        = height * 0.4 //  Пикселей в единице z
+	angle         = math.Pi / 6 //Углы осей x, y (=30 градусов)
 )
 
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
@@ -39,11 +39,11 @@ func corner(i, j int) (float64, float64) {
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
-	z := f(x, y)
+	z := hill(x, y)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	sy := height/2 + (x+y)*sin30*xyscale + z*zscale
 	return sx, sy
 }
 
@@ -52,7 +52,17 @@ func f(x, y float64) float64 {
 	if r == 0 {
 		return 0
 	}
-	sin := math.Sin(r) / r
+ 	sin := math.Sin(r) / r
 	return sin
 
+}
+
+
+func hill(x, y float64) float64 {
+	r := math.Hypot((math.Pi*x)/100, (math.Pi*y)/100)
+	if r == 0{
+		return 0
+	}
+	sin := math.Sin(r)
+	return sin
 }
